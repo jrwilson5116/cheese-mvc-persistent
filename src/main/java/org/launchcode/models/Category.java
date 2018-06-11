c9b05a9b7a3e730be3f5eb535c19e07aa7ef3c9c
jrwilson5116 <jrwilson5116@gmail.com>
2018-06-11 18:52:20 -0500

    finished up stage 2 and now I'm adding the Model class

--- src/main/java/org/launchcode/models/Category.java
+++ src/main/java/org/launchcode/models/Category.java
@@ -1,10 +1,12 @@
 package org.launchcode.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import org.hibernate.annotations.JoinColumnOrFormula;
+
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Category {
@@ -15,6 +17,10 @@ public class Category {
     @NotNull
     @Size(min=3, max= 15)
     private String name;
+
+    @OneToMany
+    @JoinColumn(name="category_id")
+    private List<Cheese> cheeses = new ArrayList<>();
 
 
     public Category(){ }
@@ -35,6 +41,12 @@ public class Category {
     public void setName(String name){
         this.name = name;
     }
+
+    public String toString(){
+        return name;
+    }
+
+
 
 
 
