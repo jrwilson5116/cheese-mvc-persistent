8e7f465f4d2208d98dfc0a5a05ee6d0869cfcc43
jrwilson5116 <jrwilson5116@gmail.com>
2018-06-11 19:19:10 -0500

    updating Menu...accidentally named it Model

--- src/main/java/org/launchcode/models/Cheese.java
+++ src/main/java/org/launchcode/models/Cheese.java
@@ -1,11 +1,11 @@
 package org.launchcode.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import org.launchcode.models.Menu;
+
+import java.util.List;
 
 /**
  * Created by LaunchCode
@@ -25,10 +25,11 @@ public class Cheese {
     @Size(min=1, message = "Description must not be empty")
     private String description;
 
-//    private CheeseType type; replacing type with category
     @ManyToOne
     private Category category;
 
+    @ManyToMany(mappedBy ="cheeses")
+    private List<Menu> menus;
 
     public Cheese(String name, String description) {
         this.name = name;
