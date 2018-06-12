a58b43f41701ceebe6105b69cb01dfbd56cfe90f
jrwilson5116 <jrwilson5116@gmail.com>
2018-06-11 19:29:54 -0500

    had to add git add Menu

---
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -0,0 +1,28 @@
+package org.launchcode.controllers;
+
+import org.launchcode.models.data.CheeseDao;
+import org.launchcode.models.data.MenuDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping(value = "menu")
+public class MenuController {
+
+    @Autowired
+    private MenuDao menuDao;
+
+    @Autowired
+    private CheeseDao cheeseDao;
+
+
+    @RequestMapping(value="")
+    public String index(Model model){
+
+
+        return "menu/index";
+    }
+}
